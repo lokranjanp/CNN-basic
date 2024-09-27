af8bc21d702f150adef198627862fe69bf0e4529
lokranjanp <lokranjan03@gmail.com>
2024-09-27 11:03:00 +0530

    understand padding in conv res

--- conv_basic.py
+++ conv_basic.py
@@ -9,9 +9,12 @@ print("Mat_A = \n", mat_A)
 kernel_matrix = np.random.randint(low=0, high=5, size=(3,3))
 print("Kernel/Filter = \n", kernel_matrix)
 
-conv_res_mat = convolve(mat_A, kernel_matrix, mode="same")
+conv_method_choice = np.random.choice(["same", "valid"])
+
+conv_res_mat = convolve(mat_A, kernel_matrix, mode=conv_method_choice)
 print("Resultant Convolutional Matrix : \n",conv_res_mat)
 
+print("Convolution method choice : ", conv_method_choice)
 print("Shape of Matrix A : ", mat_A.shape)
 print("Shape of kernel matrix : ", kernel_matrix.shape)
 print("Shape of resultant matrix : ", conv_res_mat.shape)
