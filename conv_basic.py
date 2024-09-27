00425d7546d297cfbc0d49f7e3ce3f7998512dbd
lokranjanp <lokranjan03@gmail.com>
2024-09-27 10:04:49 +0530

    understand dims of res matrix

--- conv_basic.py
+++ conv_basic.py
@@ -11,3 +11,7 @@ print("Kernel/Filter = \n", kernel_matrix)
 
 conv_res_mat = convolve(mat_A, kernel_matrix, mode="same")
 print("Resultant Convolutional Matrix : \n",conv_res_mat)
+
+print("Shape of Matrix A : ", mat_A.shape)
+print("Shape of kernel matrix : ", kernel_matrix.shape)
+print("Shape of resultant matrix : ", conv_res_mat.shape)
